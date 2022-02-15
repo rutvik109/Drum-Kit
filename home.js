70b4ba1b70c0b4fbcc5313a6969bf6b44b102844
Rutvik <rutvikpatel1092@gmail.com>
2022-02-15 17:47:54 +0530

    some changes

--- home.js
+++ home.js
@@ -80,33 +80,3 @@ function buttonAnimation(currentKey) {
 }
 
 
-// window.addEventListener('keydown',function(e){
-
-//   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-//   console.log(audio)
- 
-//   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-//   console.log(key)
-
-
-//   if (!audio){
-//     return 
-//   }
-//   audio.currentTime = 0 
-//   audio.play()
-//   key.classList.add('playing')
-// });
-
-// const removeTransition = function(e){
-//   if(e.propertyName !== 'transform')
-//    return;
-//   this.classList.remove("playing")
-// };
-
-// const keys = document.querySelectorAll(".keys");
-// console.log(keys)
-// keys.forEach(function(key){
-//   key.addEventListener("transitionend",removeTransition)
-// });
-
-
